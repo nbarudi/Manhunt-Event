f5e2d58ecd7fd047649d24a22de8d566a79b66ae
nbarudi <nbarudi@outlook.com>
2022-01-13 01:51:04 -0500

    Initial commit

---
+++ src/main/java/ca/bungo/manhunt/Manhunt.java
@@ -0,0 +1,18 @@
+package ca.bungo.manhunt;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Manhunt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
